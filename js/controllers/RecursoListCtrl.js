880c21fd0ea6128382532da63861119cdb2075a3
Rafael Escrich <rafaelescrich@gmail.com>
2016-07-12 13:57:16 -0300

    Error in RecursoListCtrl

--- js/controllers/RecursoListCtrl.js
+++ js/controllers/RecursoListCtrl.js
@@ -2,8 +2,7 @@
     'use strict';
 
     angular.module('RecursoListCtrl', [])
-      .controller('RecursoListController',
-        function($scope, $stateParams, Recursos) {
+      .controller('RecursoListController', function($scope, $stateParams, Recursos) {
 
           Recursos.getAll(function(data) {
             console.log(data);
